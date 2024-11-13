68e04dc7ef4b2040837fcf3b6c7198d2bbf1125f
mierzynski <mmierzynski@icloud.com>
2024-11-13 17:42:44 +0100

    first commit

---
+++ InteractiveMapControl/cControl/MapControl.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace InteractiveMapControl.cControl
+{
+    partial class MapControl
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapControl));
+            this.boardPanel = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.AddObjectTest = new System.Windows.Forms.ToolStripButton();
+            this.axisYPanel = new System.Windows.Forms.Panel();
+            this.axisXPanel = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // boardPanel
+            // 
+            this.boardPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.boardPanel.Location = new System.Drawing.Point(33, 28);
+            this.boardPanel.Name = "boardPanel";
+            this.boardPanel.Size = new System.Drawing.Size(551, 304);
+            this.boardPanel.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddObjectTest});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(594, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // AddObjectTest
+            // 
+            this.AddObjectTest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.AddObjectTest.Image = ((System.Drawing.Image)(resources.GetObject("AddObjectTest.Image")));
+            this.AddObjectTest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AddObjectTest.Name = "AddObjectTest";
+            this.AddObjectTest.Size = new System.Drawing.Size(23, 22);
+            this.AddObjectTest.Text = "toolStripButton1";
+            this.AddObjectTest.Click += new System.EventHandler(this.AddObjectTest_Click);
+            // 
+            // axisYPanel
+            // 
+            this.axisYPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.axisYPanel.Location = new System.Drawing.Point(7, 28);
+            this.axisYPanel.Name = "axisYPanel";
+            this.axisYPanel.Size = new System.Drawing.Size(25, 304);
+            this.axisYPanel.TabIndex = 4;
+            // 
+            // axisXPanel
+            // 
+            this.axisXPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.axisXPanel.Location = new System.Drawing.Point(33, 329);
+            this.axisXPanel.Name = "axisXPanel";
+            this.axisXPanel.Size = new System.Drawing.Size(551, 25);
+            this.axisXPanel.TabIndex = 5;
+            // 
+            // MapControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axisXPanel);
+            this.Controls.Add(this.axisYPanel);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.boardPanel);
+            this.Name = "MapControl";
+            this.Size = new System.Drawing.Size(594, 370);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel boardPanel;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton AddObjectTest;
+        private System.Windows.Forms.Panel axisYPanel;
+        private System.Windows.Forms.Panel axisXPanel;
+    }
+}
---
+++ InteractiveMapControl/cControl/MapControl.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace InteractiveMapControl.cControl
+{
+    public partial class MapControl : UserControl
+    {
+        private Point _dragStartPoint;
+        private Control _draggedControl;
+        private List<Panel> mapObjects = new List<Panel>(); // Lista przechowująca obiekty
+        private Panel _selectedPanel;
+
+        public MapControl()
+        {
+            InitializeComponent();
+
+            boardPanel.AutoScroll = true;
+            boardPanel.BackColor = Color.LightGray;
+
+            axisXPanel.Paint += AxisXPanel_Paint;
+            axisYPanel.Paint += AxisYPanel_Paint;
+        }
+
+        public void AddObject(string label, int width, int height, int x, int y)
+        {
+            var rect = new Panel
+            {
+                Width = width,
+                Height = height,
+                BackColor = Color.LightBlue,
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(x, y)
+            };
+
+            var labelControl = new Label
+            {
+                Text = label,
+                AutoSize = true,
+                Location = new Point(5, 5)
+            };
+
+            rect.Controls.Add(labelControl);
+
+            rect.MouseDown += Rectangle_MouseDown;
+            rect.MouseMove += Rectangle_MouseMove;
+            rect.MouseUp += Rectangle_MouseUp;
+
+            // Obsługa podwójnego kliknięcia
+            rect.DoubleClick += Rectangle_DoubleClick;
+
+            mapObjects.Add(rect);
+
+            boardPanel.Controls.Add(rect);
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseEventArgs e)
+        {
+            _draggedControl = sender as Control;
+            _dragStartPoint = e.Location;
+        }
+
+        private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_draggedControl != null && e.Button == MouseButtons.Left)
+            {
+                // Obliczanie nowej pozycji obiektu
+                _draggedControl.Left += e.X - _dragStartPoint.X;
+                _draggedControl.Top += e.Y - _dragStartPoint.Y;
+            }
+        }
+
+        private void Rectangle_MouseUp(object sender, MouseEventArgs e)
+        {
+            _draggedControl = null;
+        }
+
+        private void Rectangle_DoubleClick(object sender, EventArgs e)
+        {
+            var clickedPanel = sender as Panel;
+
+            if (clickedPanel != null)
+            {
+                // Jeśli mamy już zaznaczony obiekt, przywróć jego oryginalny kolor
+                if (_selectedPanel != null)
+                {
+                    _selectedPanel.BackColor = Color.LightBlue; // Domyślny kolor
+                }
+
+                // Podświetl kliknięty obiekt
+                clickedPanel.BackColor = Color.LightGreen;
+                _selectedPanel = clickedPanel; // Zapamiętaj zaznaczony obiekt
+
+                MessageBox.Show($"Kliknięto obiekt: {clickedPanel.Controls[0].Text}");
+
+                _selectedPanel.BackColor = Color.LightBlue;
+            }
+        }
+
+        private void DrawAxis(Graphics g, bool isXAxis)
+        {
+            Pen thickPen = new Pen(Color.Black, 2);
+            Pen thinPen = new Pen(Color.Black, 1);
+            Font labelFont = new Font("Arial", 6);
+
+            int panelWidth = isXAxis ? axisXPanel.Width : axisYPanel.Width;
+            int panelHeight = isXAxis ? axisXPanel.Height : axisYPanel.Height;
+
+            int spacing = 20;
+
+            // Rysowanie głównej linii osi X (u dołu) lub Y (po lewej)
+            if (isXAxis)
+            {
+                g.DrawLine(thinPen, 0, panelHeight - 1, panelWidth, panelHeight - 1);
+            }
+            else
+            {
+                g.DrawLine(thinPen, 0, 0, 0, panelHeight);
+            }
+
+            // Rysowanie głównych kresek i etykiet
+            for (int i = 0; i <= (isXAxis ? panelWidth : panelHeight) / spacing; i++)
+            {
+                int position = i * spacing;
+
+                if (isXAxis)
+                {
+                    // Rysowanie głównej kreski na osi X
+                    g.DrawLine(thickPen, position, panelHeight - 10, position, panelHeight);
+
+                    // Rysowanie cienkich kresek na osi X
+                    if (i > 0)
+                    {
+                        int thinPosition = position - spacing / 2;
+                        g.DrawLine(thinPen, thinPosition, panelHeight - 5, thinPosition, panelHeight);
+                    }
+
+                    // Etykiety na osi X
+                    if (i > 0)
+                    {
+                        string label = i.ToString();
+                        SizeF labelSize = g.MeasureString(label, labelFont);
+                        float labelX = position - (labelSize.Width / 2);
+                        float labelY = panelHeight - 20;
+                        g.DrawString(label, labelFont, Brushes.Black, labelX, labelY);
+                    }
+                }
+                else
+                {
+                    // Rysowanie głównej kreski na osi Y
+                    g.DrawLine(thickPen, 0, position, 10, position);
+
+                    // Rysowanie cienkich kresek na osi Y
+                    if (i > 0)
+                    {
+                        int thinPosition = position - spacing / 2;
+                        g.DrawLine(thinPen, 0, thinPosition, 5, thinPosition);
+                    }
+
+                    // Etykiety na osi Y, zmniejszona odległość od osi
+                    if (i > 0)
+                    {
+                        string label = i.ToString();
+                        SizeF labelSize = g.MeasureString(label, labelFont);
+                        float labelX = 12; // Mniejsza wartość dla osi Y, aby etykiety były bliżej kreski
+                        float labelY = position - (labelSize.Height / 2);
+                        g.DrawString(label, labelFont, Brushes.Black, labelX, labelY);
+                    }
+                }
+            }
+        }
+
+        private void AxisXPanel_Paint(object sender, PaintEventArgs e)
+        {
+            DrawAxis(e.Graphics, isXAxis: true);
+        }
+
+        private void AxisYPanel_Paint(object sender, PaintEventArgs e)
+        {
+            DrawAxis(e.Graphics, isXAxis: false);
+        }
+
+        private void AddObjectTest_Click(object sender, EventArgs e)
+        {
+            AddObject("Test Object", 100, 50, 50, 50); // Przykładowy obiekt
+        }
+    }
+}
